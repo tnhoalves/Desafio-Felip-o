bd5f1a65d1a94e318e8800aafaed7bad0e07ca88
tnhoalves <tnhoalves@gmail.com>
2023-12-01 18:39:48 -0300

    meu Desafio completo

---
+++ index.js
@@ -0,0 +1,26 @@
+//definindo as variaveis,
+let nomeDoHeroi = "luca";
+let xpdoHeroi = "40";
+let raquedoHeroi = xpdoHeroi;
+
+if (raquedoHeroi <= 999) {
+    xpdoHeroi = "Unranqued"
+  } else if (raquedoHeroi <=1000) {
+    xpdoHeroi = "Ferro"
+  } else if (raquedoHeroi <= 2000) {
+    xpdoHeroi = "Bronze"
+  } else if (raquedoHeroi <= 6000) {
+    xpdoHeroi = "Prata"
+  } else if (raquedoHeroi <= 7000) {
+    xpdoHeroi = "Ouro"
+  } else if (raquedoHeroi <= 8000) {
+    xpdoHeroi = "Platina"
+  } else if (raquedoHeroi <= 9000) {
+    xpdoHeroi = "Ascendente"
+  } else if (raquedoHeroi <= 10000) {
+    xpdoHeroi = "Imortal"
+  } else if (raquedoHeroi >= 10001) {
+    xpdoHeroi = "Radiante"
+  }
+
+  console.log("O nome do Heroi é: " + nomeDoHeroi + ". Seu nivel é: "+ xpdoHeroi)
